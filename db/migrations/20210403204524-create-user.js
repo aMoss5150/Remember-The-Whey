7d38626a001b7c181dfcacf2f50473888ff45d94
earlwoo <earl.woo@gmail.com>
2021-04-03 17:26:19 -0400

    skeleton completed, models made and associated

---
+++ db/migrations/20210403204524-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      hashPW: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ db/models/task.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Task = sequelize.define('Task', {
+    name: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN,
+    date: DataTypes.DATE,
+    notes: DataTypes.TEXT,
+    listId: DataTypes.INTEGER,
+    sets: DataTypes.INTEGER,
+    reps: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER
+  }, {});
+  Task.associate = function(models) {
+    Task.belongsTo(models.List, { foreignKey: 'listsId' })
+    // associations can be defined here
+  };
+  return Task;
+};
