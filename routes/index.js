1d079a549fadb7321f3467f0e47d4a5a28d3f0a9
earlwoo <earl.woo@gmail.com>
2021-04-05 19:33:05 -0400

    setting up files

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // session? if no resredirect to login
-
-  // if session res.render('index')
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
-// router.get('/log-in' )
-//   res.render('log-in')
 
 
-router.get('/sign-up', (req, res) => {
-  res.render('sign-up')
-})
-
-router.post('/sign-up', )
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const { asyncHandler, csrfProtection } = require('./utils')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/log-in', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/log-in')
+
+router.get('/sign-up', function(req, res, next) {
   res.send('respond with a resource');
 });
 
