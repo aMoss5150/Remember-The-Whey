e970f442a9dce2113832f8b0ec2a11c2b6ea3408
earlwoo <earl.woo@gmail.com>
2021-04-05 18:44:28 -0400

    made sign-up form

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  // session? if no resredirect to login
+
+  // if session res.render('index')
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
+
+// router.get('/log-in' )
+//   res.render('log-in')
+
+
+router.get('/sign-up', (req, res) => {
+  res.render('sign-up')
+})
+
+router.post('/sign-up', )
+
+
 
 module.exports = router;
