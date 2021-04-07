7154dcc3773591ce7fa054d8ab337dbf2ebd21e3
Andrew Moss <andrewmossaudio@gmail.com>
2021-04-07 16:01:32 -0500

    working slide summary div

---
+++ public/javascripts/slideout.js
@@ -0,0 +1,25 @@
+
+let slideout = document.querySelector('.summary__slideout');
+
+function openSlideout() {
+    console.log('banana');
+    slideout.style.width = '250px';
+    // document.querySelector('#main').style.marginLeft = "250px";
+}
+
+// console.log(openSlideout);
+
+function closeSlideout() {
+
+    slideout.style.width = '0';
+    // document.querySelector('#main').style.marginLeft = "0";
+}
+
+slideout.addEventListener('click', () => {
+
+    //checking id status, if so, run 
+})
+console.log(slideout)
+
+
+
