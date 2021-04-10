febbe169d1525154b96f6e545ecb1d872f537157
earlwoo <earl.woo@gmail.com>
2021-04-10 18:01:21 -0400

    made changes to search

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,48 +1,30 @@
-//implement a ajax call to fetch all task which meet a regex search criteria
-// window.addEventListener("DOMContentedLoaded", (e) => {
-// when a search is entered
+///// Menu open/exit/hide/show //////////////
 
 
-
-////////////////////////////////         CODE FOR SEARCH                ///////////////////            REFACTOR WITH MICHAEL             //////////////////////////////
-const searchBtn = document.querySelector(".search__submit");
+const searchExpandBtn = document.querySelector(".search__button--extend");
 const searchInp = document.querySelector(".navbar__search_input");
+const xIconExtended = document.querySelector(".x-icon")
+const extendSearchDiv = document.querySelector('#search_bar-extend')
 
-searchBtn.addEventListener('click', async (e) => {
+searchExpandBtn.addEventListener('click', async (e) => {
     //prevent refresh of page
     e.preventDefault()
+    if(extendSearchDiv.classList.contains("search_bar-extend--hide")) {
+        extendSearchDiv.classList.remove("search_bar-extend--hide")
+        extendSearchDiv.classList.add("search_bar-extend--show")
 
-    // grab input from search input to use as the regex
-    const searchIncTerm = searchInp.value;
-    const searchExcTerm = '';
-    const fieldToSearch = 'name';
-    // need to grab html element where i can display all tasks
-    // with make variable let tasksContainer === html display element
-
-    try {
-        // json should have ALL Tasks belonging to user (authenticated and authorized in backend)
-        let tasks = await getTasks();
-
-        tasks = await filterTasks(tasks, {
-            include: {
-                term: searchIncTerm,
-                from: fieldToSearch
-            }
-        });
-
-        if (tasks.length === 0) {
-            alert("No tasks matched your search");
-        }
-        else {
-            // display results on task section display area
-            await displayTasks(tasks);
-        }
+    } else {
+        extendSearchDiv.classList.remove(`search_bar-extend--show`)
+        extendSearchDiv.classList.add(`search_bar-extend--hide`)
     }
-    catch (err) {
-        // display error somewhere in html
-        console.log(err)
-    }
+
 })
+
+xIconExtended.addEventListener('click', e => {
+    extendSearchDiv.classList.remove(`search_bar-extend--show`)
+    extendSearchDiv.classList.add(`search_bar-extend--hide`)
+})
+
 
 const hamburger = document.querySelector('.navbar__open--slide')
 hamburger.addEventListener('click', e => {
@@ -65,4 +47,70 @@ hamburger.addEventListener('click', e => {
     }
 
 })
-// })
+
+
+///////   Search and Extended Search /////////
+const btnExtSubmit = document.querySelector('.extended_search-btn')
+const navSearch = document.querySelector('.navbar__search_input')
+
+searchInp.addEventListener('keypress', async(e) => {
+    if (e.key === 'Enter') {
+        const searchIncTerm = navSearch.value || null;
+        console.log(searchIncTerm)
+        try {
+            let tasks = await getTasks();
+
+            tasks = await filterTasks(tasks, {
+                include: {
+                    term: searchIncTerm,
+                }
+            });
+
+            if (tasks.length === 0) {
+                alert("No tasks matched your search");
+            } else {
+                await displayTasks(tasks);
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+})
+
+
+const searchIncInp = document.querySelector('.extSearch__form--searchIncTerm')
+const searchExcInp = document.querySelector('.extSearch__form--searchExcTerm')
+const searchNotesInp = document.querySelector('.extSearch__form--notes')
+
+btnExtSubmit.addEventListener('click', async(e) => {
+    e.preventDefault()
+    const searchIncTerm = searchIncInp.value || null;
+    console.log(searchIncTerm)
+    const searchExcTerm = searchExcInp.value || null;
+    console.log(searchExcTerm)
+    const includeNotes = searchNotesInp.checked;
+    console.log(includeNotes)
+    try {
+        let tasks = await getTasks();
+
+        tasks = await filterTasks(tasks, {
+            include: {
+                term: searchIncTerm,
+                includeNotes
+            },
+            exclude: {
+                term: searchExcTerm,
+                includeNotes
+            }
+        });
+
+        if (tasks.length === 0) {
+            alert("No tasks matched your search");
+        } else {
+            await displayTasks(tasks);
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+})
