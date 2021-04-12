88c3c1f14230afdfd8dd5d0d5a7138e8fbcac706
Andrew Moss <andrewmossaudio@gmail.com>
2021-04-11 20:31:39 -0500

    merging with hector

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,4 +1,3 @@
-import { closeSlideout, openSlideout } from "/javascripts/slideout.js"
 
 
 const utilsIncomplete = document.querySelector('#utils__incomplete');
@@ -684,6 +683,33 @@ const taskSelectHandler = async (ev) => {
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //!--start--SUMMARY SECTION ANDREW
+
+//!SLIDEOUT CONSOLIDATED
+let slideout = document.querySelector('.summary__slideout')
+let slideoutBtn = document.querySelector('.slideout__closebtn');
+function openSlideout() {
+    slideout.classList.remove("hidden__slide")
+    slideout.classList.add("shown__slide")
+}
+
+function closeSlideout() {
+    slideout.classList.add("hidden__slide")
+    slideout.classList.remove('shown__slide')
+}
+
+slideoutBtn.addEventListener('click', closeSlideout)
+//MAKE SURE TO IMPLEMENT ID CHANGE TO BE ABLE TO TOGGLE SINCE
+//I WILL NOT BE ABLE TO USE THE ONCLICK EVENT!
+//!TEST SLIDEOUT
+
+// const testButton = document.querySelector('#button__test')
+
+// testButton.addEventListener("click", () => {
+//     closeSlideout()
+// })
+
+
+
 console.log('closeSlideout:', closeSlideout)
 document.addEventListener('DOMContentLoaded', () => {
     //!.SUMMARY ELEMENTS
